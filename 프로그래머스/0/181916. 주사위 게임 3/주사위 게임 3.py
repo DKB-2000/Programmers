f1b29079e2535929f7a8cc6696c9185dbae2aa39
D_KB_2000 <106220288+DKB-2000@users.noreply.github.com>
2026-09-20 17:02:51 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.01 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
@@ -1,50 +1,45 @@
-
 def solution(a, b, c, d):
     answer = 0
-    dice = {}
-    
-    for i in [a,b,c,d]:
-        if i in dice:
-            dice[i] += 1
-        else:
-            dice[i] = 1
-    p,q,r = 0,0,0
-    if len(dice.items()) == 1:
-        for key,value in dice.items():
-            answer = key * 1111
 
-    if len(dice.items()) == 2:
-        for key,value in dice.items():
-            if value == 3:
-                p = key
-            if value == 1:
-                q = key
-        answer = (10*p+q) ** 2
+    mdic = dict()
+    mm,mn = 0,0
+    for i in [a,b,c,d]:
+        if i not in mdic.keys():
+            mdic[i] = 1
+        else:
+            mdic[i] += 1
+
+    tmp = list(mdic.keys())
+
+    tmp_item = list(mdic.items())
+    
+    if len(tmp) == 1:
+        answer = tmp[0] * 1111
         
-        if answer == 0:
-            for idx, (key, value) in enumerate(dice.items()):
-                if idx == 0:
-                    p = key
-                if idx == 1:
-                    q = key
-            answer = (p+q) * abs(p-q)
+    if len(tmp) == 2:
+        for i,v in tmp_item:
+            if v == 3:
+                mm = i
+            if v == 1:
+                mn = i
+        answer = (10*mm + mn) ** 2
+        
+        if mm == 0 and mn == 0:
+            answer = (tmp[0] + tmp[1]) * abs(tmp[0]-tmp[1])
             
-    if len(dice.items()) == 3:
-        for key, value in dice.items():
-            if value == 2:
-                p = key
-            if value == 1:
-                if q == 0:
-                    q = key
-                else:
-                    r = key
-                    
-        answer = q * r
-        
-    if len(dice.items()) == 4:
-        answer = min(a,b,c,d)
-    
+            
+    if len(tmp) == 3:
+        candidate = []
+        for i,v in tmp_item:
+            if v == 2:
+                continue
+            else:
+                candidate.append(i)
                 
-    
-    
+        answer = candidate[0] * candidate[1]
+
+    if len(tmp) == 4:
+        answer = min(tmp)
+
+        
     return answer
